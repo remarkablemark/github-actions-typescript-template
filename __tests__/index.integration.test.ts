24f4aae60007c7348e011ed3aa111e4ed91ca853
Mark <mark@remarkablemark.org>
2023-04-03 01:52:53 -0400

    test(integration): refactor test

--- __tests__/index.integration.test.ts
+++ __tests__/index.integration.test.ts
@@ -1,11 +1,14 @@
 import { execFileSync } from 'child_process';
 import { resolve } from 'path';
 
+const nodePath = process.execPath;
+const actionPath = resolve(__dirname, '../dist/index.js');
+
 it('runs action', () => {
-  process.env.INPUT_VERSION = '1.2.3';
-  expect(
-    execFileSync(process.execPath, [resolve(__dirname, '../dist/index.js')], {
-      env: process.env,
-    }).toString()
-  ).toContain(`::debug::version: ${process.env.INPUT_VERSION}`);
+  const version = '1.2.3';
+  process.env.INPUT_VERSION = version;
+  const buffer = execFileSync(nodePath, [actionPath], {
+    env: process.env,
+  });
+  expect(buffer.toString()).toContain(`::debug::version: ${version}`);
 });
