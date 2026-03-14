5f59e1d579e5e995cddd00070562d39e1d4ad37b
Mark <mark@remarkablemark.org>
2026-03-14 11:44:05 -0400

    build(tsup): remove 4 redundant fields (format, dts, outDir, clean)

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,11 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['cjs'],
-  dts: false,
   minify: true,
   target: 'node24',
-  outDir: 'dist',
-  clean: true,
   noExternal: [/./],
 });
